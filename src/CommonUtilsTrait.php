7e51ae18aea3241de204e2aea94c322874f35dea
Bianka Martinovic <info@webbird.de>
2021-06-29 11:02:11 +0200

    replaced mt_rand() with random_int()

--- src/CommonUtilsTrait.php
+++ src/CommonUtilsTrait.php
@@ -17,7 +17,7 @@ trait CommonUtilsTrait
         if (function_exists('com_create_guid') === true) {
             return trim(com_create_guid(), '{}');
         }
-        return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+        return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', random_int(0, 65535), random_int(0, 65535), random_int(0, 65535), random_int(16384, 20479), random_int(32768, 49151), random_int(0, 65535), random_int(0, 65535), random_int(0, 65535));
     }   // end function createGUID()
 
     /**
